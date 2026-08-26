8bcadfb71417cc03bd10254dae0d72b8c8982e62
Njall Skarphedinsson <42113680+njallskarp@users.noreply.github.com>
2026-08-26 08:02:14 -0400

    Add in-memory knowledge graph index

--- src/discovery_net/indexing/__init__.py
+++ src/discovery_net/indexing/__init__.py
@@ -1,1 +1,5 @@
 # Components that project committed artifacts into rebuildable indexes.
+
+from discovery_net.indexing.knowledge_graph_index import IndexedArtifact, KnowledgeGraphIndex
+
+__all__ = ["IndexedArtifact", "KnowledgeGraphIndex"]
